a8709067de36c54e2bb518817e10982142d57cb6
Mumtaz Enser <98307861+MumtazEnser@users.noreply.github.com>
2026-05-14 21:56:11 +0300

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,7 @@
+
+function toggleTheme(){
+const html = document.documentElement;
+const current = html.getAttribute('data-theme');
+const next = current === 'dark' ? 'light' : 'dark';
+html.setAttribute('data-theme', next);
+}
