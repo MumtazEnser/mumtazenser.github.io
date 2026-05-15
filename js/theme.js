f831b250322ea111d6f070a6148d48f83f5e4d83
Mumtaz Enser <98307861+MumtazEnser@users.noreply.github.com>
2026-05-15 23:06:04 +0300

    Delete js directory

--- js/theme.js
+++
@@ -1,6 +0,0 @@
-
-function toggleTheme(){
-  const root = document.documentElement;
-  const current = root.getAttribute('data-theme') || 'dark';
-  root.setAttribute('data-theme', current === 'dark' ? 'light' : 'dark');
-}
